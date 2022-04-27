7c7cf179060cf560d6a98ad4cb571bb6ae252448
AniketAjianthSuryawa <aniket.s@nighthack.in>
2022-04-27 14:42:31 +0530

    integrate a whats app api for mantra

--- Application1/views.py
+++ Application1/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import User, Places
 from .serializer import UserSerializer, PlacesSerializer
 from rest_framework.decorators import action
-from utils import  custom_viewsets
+from utils import custom_viewsets
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
@@ -16,8 +16,10 @@ from utils.custome_permissions import IsEndUser
 from utils.utils import user_object
 
 # Create your views here.
+
+
 class UserViewSetAPIView(custom_viewsets.ModelViewset):
-    model = User 
+    model = User
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
@@ -46,7 +48,7 @@ class UserViewSetAPIView(custom_viewsets.ModelViewset):
 
         if self.action == 'list':
             permission_classes = [IsEndUser]
-            return [permission() for permission in permission_classes]    
+            return [permission() for permission in permission_classes]
         return super().get_permissions()
 
     @action(detail=False, methods=['POST'])
@@ -58,7 +60,8 @@ class UserViewSetAPIView(custom_viewsets.ModelViewset):
         payload['username'] = user.name
         payload['id'] = user.id
         token = jwt_encode_handler(payload)
-        expiration = datetime.utcnow() + settings.JWT_AUTH['JWT_EXPIRATION_DELTA']
+        expiration = datetime.utcnow(
+        ) + settings.JWT_AUTH['JWT_EXPIRATION_DELTA']
         expiration_epoch = expiration.timestamp()
 
         WhiteListedJWTTokenUtil.create_token(user, token)
@@ -71,21 +74,18 @@ class UserViewSetAPIView(custom_viewsets.ModelViewset):
         }
         return Response(data, status=status.HTTP_200_OK)
 
-    @action(detail=False, methods=['GET']) 
+    @action(detail=False, methods=['GET'])
     def user_data(self, request):
         user = user_object(request)
 
         if not user:
             raise ValidationError("User not found !")
 
-            
-
-        serializer = self.get_serializer(user) 
+        serializer = self.get_serializer(user)
         self.data.update({"data": serializer.data,
                           "message": self.retrieve_success_message})
 
-        return Response(self.data, status=status.HTTP_200_OK)                         
-
+        return Response(self.data, status=status.HTTP_200_OK)
 
 
 class PlacesViewSetAPIView(custom_viewsets.ModelViewset):
@@ -98,18 +98,76 @@ class PlacesViewSetAPIView(custom_viewsets.ModelViewset):
     retrieve_success_message = 'Places Information returned successfully!'
     update_success_message = 'Places Information updated successfully!'
 
+    def get_permissions(self):
+        if self.action in ['create', 'get_user_places']:
+            permission_classes = [IsEndUser]
+            return [permission() for permission in permission_classes]
+
+        if self.action in ['whats_app_user']:
+            permission_classes = [AllowAny]
+            return [permission() for permission in permission_classes]
+
+        if self.action == 'retrieve':
+            permission_classes = [IsEndUser]
+            return [permission() for permission in permission_classes]
+
+        if self.action == 'list':
+            permission_classes = [IsEndUser]
+            return [permission() for permission in permission_classes]
+        return super().get_permissions()
 
     @action(detail=False, methods=['GET'])
-    def get_user_places(self,request):
+    def get_user_places(self, request):
 
         place_id = self.request.data.get('place_id')
 
         places = self.get_queryset().filter(id=place_id).first()
-        
+
         if not places:
             raise ValidationError("Places not fond !")
 
         places.user.username = "Aniket Suryawanhsi"
         places.save()
         serializer = self.get_serializer(places)
-        return Response(serializer.data,status=status.HTTP_200_OK)    
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @action(detail=False, methods=['GET'])
+    def whats_app_user(self, request):
+        import http.client
+        import json
+
+        authorization = 'App c3c6c881ee9ecc553da82d7b4ebbaedc-edaeb328-541a-4548-ac7b-f6f7c9d34e48'
+        baseurl = "29y9z.api.infobip.com"
+        conn = http.client.HTTPSConnection(f"{baseurl}")
+        payload = json.dumps({
+            "messages": [
+                {
+                    "from": "916366442244",
+                    "to": "917038206592",
+                    "messageId": "a28dd97c-1ffb-4fcf-99f1-0b557ed381da",
+                    "content": {
+                        "templateName": "tollfreenumber",
+                        "templateData": {
+                            "body": {
+                                "placeholders": [
+                                    "test test"
+                                ]
+                            }
+                        },
+                        "language": "en"
+                    },
+                    "callbackData": "Callback data",
+                    "notifyUrl": "https://www.example.com/whatsapp"
+                }
+            ]
+        })
+        headers = {
+            'Authorization': f'{authorization}',
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+        conn.request("POST", "/whatsapp/1/message/template", payload, headers)
+        res = conn.getresponse()
+        data = res.read()
+
+        return Response(data.decode("utf-8"), status=status.HTTP_200_OK)
